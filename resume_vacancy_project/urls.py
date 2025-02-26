11806879f85d5b6b92145d5622c34d645958b0f3
biwanun1690 <annapriya108@gmail.com>
2025-02-26 22:18:08 +0300

    Update urls.py

--- resume_vacancy_project/urls.py
+++ resume_vacancy_project/urls.py
@@ -16,10 +16,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from resume_vacancy_app.views import index_page
+from resume_vacancy_app.views import *
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_page),
+    path('', index_page, name='index'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('signup/', sign_up, name='signup'),
+    path('create_resume/', create_resume, name='create_resume'),
+    path('list_resumes/', list_resumes, name='list_resumes'),
+    #path('list_resumes/<int:pk>', view_resume, name='view_resume'),
+    path('list_resumes/recomendation/<int:pk>', recomendation_page, name='recomendations'),
+    path('my_resume/', view_my_resume, name='my_resume'),
+    path('my_resume/<int:pk>', view_my_resume_detal, name='my_resume_detal'),
+    path('my_resume/edit/<int:pk>', edit_resume, name='edit_resume'),
+    path('my_resume/delete/<int:pk>/', delete_resume, name='delete_resume'),
+    path('list_vacancies/', list_vacancies, name='list_vacancies'),
+    path('list_vacancies/<int:pk>', view_vacancy, name='vacancy'),
+    path('list_vacancies/comments/<int:pk>', comment_page, name='comments'),
+    path('list_vacancies/complains/<int:pk>', complain_page, name='complains'),
+    path('list_vacancies/toggle_favorite/<int:pk>', toggle_favorite, name='toggle_favorite'),
+    path('favorites/', view_favorite_vacancies, name='favorites_vacancies'),
+    path('my_resume/need_vacancies/<int:pk>', vacancies_for_resume, name='need_vacancies'),
 ]
